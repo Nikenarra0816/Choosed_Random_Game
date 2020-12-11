65e634f5e3bb9733c77677f6c59b083b0640198a
Nikenarra0816 <nikenarra0816@gmail.com>
2020-12-11 12:01:03 +0700

    update router app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Minion } from "./components/Minion";
+import { Hero } from "./components/Hero";
+import { Add } from "./components/Add";
+import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
 
 function App() {
   return (
-    <div></div>
+    <Router>
+       <Header/>
+       <Switch>
+       <Route exact path="/">
+            <Hero />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route path="/Monion">
+            <Minion />
+          </Route>
+        </Switch>
+     
+    </Router>
   );
 }
 
