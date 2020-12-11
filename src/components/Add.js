1d58486298d9d37728b2bf5510d7804fc1c44a2b
Nikenarra0816 <nikenarra0816@gmail.com>
2020-12-11 12:11:13 +0700

    update add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Add = () => {
-    return (
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const onChange = (e) => {
+    e.preventDefault();
+
+    setQuery(e.target.value);
+  };
+
+  return (
     <div className="add-page">
       <div className="container">
         <div className="add-content">
           <div className="input-wrapper">
-            <input
-              type="text"
-              placeholder="Search for a movie"/>
+            <input type="text" placeholder="Search for a movie" />
           </div>
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
