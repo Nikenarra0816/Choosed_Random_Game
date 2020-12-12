66c1a0ddde4cd548d1b5203a87c3e45e6c59f94b
Nikenarra0816 <nikenarra0816@gmail.com>
2020-12-12 08:06:57 +0700

    fetch data API

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,57 +1,108 @@
-import React, { useState } from "react";
-import { ResultCard } from "../components/ResultCard";
+// import React, { useState } from "react";
+// import { ResultCard } from "../components/ResultCard";
 
-export const Add = () => {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
+// export const Add = () => {
+//   const [query, setQuery] = useState("");
+//   const [results, setResults] = useState([]);
 
-  const onChange = (e) => {
-    e.preventDefault();
+//   const onChange = (e) => {
+//     e.preventDefault();
 
-    setQuery(e.target.value);
+//     setQuery(e.target.value);
 
+//     fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards", {
+//       method: "GET",
+//       headers: {
+//         "x-rapidapi-key": "8ca2b61598mshcbf02117ba11b57p16b0b8jsn50617050e449",
+//         "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         // console.log(data);
+//         if (!data.errors) {
+//           setResults(data.results);
+//         } else {
+//           setResults([]);
+//         }
+//       });
+//   };
+
+//   return (
+//     <div className="add-page">
+//       <div className="container">
+//         <div className="add-content">
+//           <div className="input-wrapper">
+//             <input
+//               type="text"
+//               placeholder="Search for a movie"
+//               value={query}
+//               onChange={onChange}
+//             />
+//           </div>
+
+//           {results.length > 0 && (
+//             <ul className="results">
+//               {results.map((movie) => (
+//                 <li key={movie.id}>
+//                   <ResultCard movie={movie} />
+//                 </li>
+//               ))}
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+import React from "react";
+// import "./styles.css";
+
+
+
+ class Add extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      isLoading: true
+    };
+  
+  }
+
+  componentDidMount() {
     fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards", {
       method: "GET",
       headers: {
         "x-rapidapi-key": "8ca2b61598mshcbf02117ba11b57p16b0b8jsn50617050e449",
-        "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-      },
+        "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+      }
     })
       .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        if (!data.errors) {
-          setResults(data.results);
-        } else {
-          setResults([]);
-        }
+      .then((res) => {
+        this.setState({
+          data: res,
+          isLoading: false
+        });
+      })
+      .catch((err) => {
+        console.error(err);
       });
-  };
+  }
 
-  return (
-    <div className="add-page">
-      <div className="container">
-        <div className="add-content">
-          <div className="input-wrapper">
-            <input
-              type="text"
-              placeholder="Search for a movie"
-              value={query}
-              onChange={onChange}
-            />
-          </div>
+  render() {
+    const { data, isLoading } = this.state;
+    if (isLoading) return <p>Loading...</p>;
+    if (!isLoading)
+      return (
+        <React.Fragment>
+          {data.Basic.map((v) => {
+            return <p>{v.name}</p>;
+          })}
+        </React.Fragment>
+      );
+  }
+}
 
-          {results.length > 0 && (
-            <ul className="results">
-              {results.map((movie) => (
-                <li key={movie.id}>
-                  <ResultCard movie={movie} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
+export default Add;
